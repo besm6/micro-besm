f3e1a6a4aad36e11de11afa951544a5032c65fdc
Serge Vakulenko <serge.vakulenko@gmail.com>
2016-10-11 19:54:50 -0700

    Update k1802bb1 implementation and test.

--- rtl/k1802bb1.sv
+++ rtl/k1802bb1.sv
@@ -2,14 +2,14 @@
 // K1802BB1 - Multiport RAM 4x4
 //
 module k1802bb1(
-    input        [3:0] DA,      // A data bus input...
-    output logic [3:0] oDA,     // ...and output
-    input        [3:0] DB,      // B data bus input...
-    output logic [3:0] oDB,     // ...and output
-    input        [3:0] DC,      // C data bus input...
-    output logic [3:0] oDC,     // ...and output
-    input        [3:0] DX,      // X data bus input...
-    output logic [3:0] oDX,     // ...and output
+    input        [3:0] nDA,     // A data bus input...
+    output logic [3:0] onDA,    // ...and output
+    input        [3:0] nDB,     // B data bus input...
+    output logic [3:0] onDB,    // ...and output
+    input        [3:0] nDC,     // C data bus input...
+    output logic [3:0] onDC,    // ...and output
+    input        [3:0] nDX,     // X data bus input...
+    output logic [3:0] onDX,    // ...and output
 
     input        [1:0] AA,      // A address input
     input        [1:0] AB,      // B address input
@@ -31,76 +31,123 @@ module k1802bb1(
     input              nRC,     // C read enable
     input              nRX,     // X read enable
 
-    input              CI,      // counter input
-    output logic       CO,      // counter overflow
+    input              nCI,     // counter input
+    output logic       nCO,     // counter overflow
     output logic       E        // counter equal flag RG0==RG3
 );
 
 // Internal RAM.
+// Register RG0 is edge-sensitive,
+// registers RG1-RG3 are level sensitive.
 logic [3:0] RG[4];
+
+// Output bus enable.
+logic outA, outB, outC, outX;
+assign outA = !nECA & !nRA;         // bus A output enable
+assign outB = !nECB & !nRB;         // bus B output enable
+assign outC = !nECC & !nRC;         // bus C output enable
+assign outX = !nECX & !nRX;         // bus D output enable
+
+// Select signals.
+logic selA0, selA1, selA2, selA3;
+assign selA0 = !nECA & (AA == 0);   // port A selects RG0
+assign selA1 = !nECA & (AA == 1);   // port A selects RG1
+assign selA2 = !nECA & (AA == 2);   // port A selects RG2
+assign selA3 = !nECA & (AA == 3);   // port A selects RG3
+
+logic selB0, selB1, selB2, selB3;
+assign selB0 = !nECB & (AB == 0);   // port B selects RG0
+assign selB1 = !nECB & (AB == 1);   // port B selects RG1
+assign selB2 = !nECB & (AB == 2);   // port B selects RG2
+assign selB3 = !nECB & (AB == 3);   // port B selects RG3
+
+logic selC0, selC1, selC2, selC3;
+assign selC0 = !nECC & (AC == 0);   // port C selects RG0
+assign selC1 = !nECC & (AC == 1);   // port C selects RG1
+assign selC2 = !nECC & (AC == 2);   // port C selects RG2
+assign selC3 = !nECC & (AC == 3);   // port C selects RG3
+
+logic selX0, selX1, selX2, selX3;
+assign selX0 = !nECX & (AX == 0);   // port X selects RG0
+assign selX1 = !nECX & (AX == 1);   // port X selects RG1
+assign selX2 = !nECX & (AX == 2);   // port X selects RG2
+assign selX3 = !nECX & (AX == 3);   // port X selects RG3
+
+// Clock and enable signals for RGi.
 logic clk0, sel1, sel2, sel3;
 
-// Read RGi.
-assign oDA = (!nRA & !nECA) ? RG[AA] : 'z;
-assign oDB = (!nRB & !nECB) ? RG[AB] : 'z;
-assign oDC = (!nRC & !nECC) ? RG[AC] : 'z;
-assign oDX = (!nRX & !nECX) ? RG[AX] : 'z;
+assign clk0 = ((selA0 & !nWA) |     // clock for flip-flop RG0
+               (selB0 & !nWB) |
+               (selC0 & !nWC) |
+               (selX0 & !nWX)) & !nCI;
+assign en1 = (selA1 & !nWA) |       // data enable for latch RG1
+             (selB1 & !nWB) |
+             (selC1 & !nWC) |
+             (selX1 & !nWX);
+assign en2 = (selA2 & !nWA) |       // data enable for latch RG2
+             (selB2 & !nWB) |
+             (selC2 & !nWC) |
+             (selX2 & !nWX);
+assign en3 = (selA3 & !nWA) |       // data enable for latch RG3
+             (selB3 & !nWB) |
+             (selC3 & !nWC) |
+             (selX3 & !nWX);
 
-// RG0 is edge-sensitive, RG1-RG3 level sensitive.
-assign clk0 = ((AA == 0) & !nECA & !nWA) |
-              ((AB == 0) & !nECB & !nWB) |
-              ((AC == 0) & !nECC & !nWC) |
-              ((AX == 0) & !nECX & !nWX) | CI;
-assign sel1 = ((AA == 1) & !nECA) |
-              ((AB == 1) & !nECB) |
-              ((AC == 1) & !nECC) |
-              ((AX == 1) & !nECX);
-assign sel2 = ((AA == 2) & !nECA) |
-              ((AB == 2) & !nECB) |
-              ((AC == 2) & !nECC) |
-              ((AX == 2) & !nECX);
-assign sel3 = ((AA == 3) & !nECA) |
-              ((AB == 3) & !nECB) |
-              ((AC == 3) & !nECC) |
-              ((AX == 3) & !nECX);
-
-// Flip-flop RG0
+// Flip-flop RG0 and latches RG1-RG3.
+logic CO;
 always @(posedge clk0)
-    if (CI)
+    if (nCI)
         {CO, RG[0]} <= RG[0] + 1;
     else begin
-        RG[0] <= ((AA == 0) & !nWA) ? DA :
-                 ((AB == 0) & !nWB) ? DB :
-                 ((AC == 0) & !nWC) ? DC :
-                 ((AX == 0) & !nWX) ? DX : RG[0];
+        RG[0] <= ((!outA & !nWA & selA0) ? ~nDA : 0) |
+                 ((!outB & !nWB & selB0) ? ~nDB : 0) |
+                 ((!outC & !nWC & selC0) ? ~nDC : 0) |
+                 ((!outX & !nWX & selX0) ? ~nDX : 0);
         CO <= 0;
     end
+always @(*)
+    if (en1)
+        RG[1] <= ((!outA & !nWA & selA1) ? ~nDA : 0) |
+                 ((!outB & !nWB & selB1) ? ~nDB : 0) |
+                 ((!outC & !nWC & selC1) ? ~nDC : 0) |
+                 ((!outX & !nWX & selX1) ? ~nDX : 0);
+always @(*)
+    if (en2)
+        RG[2] <= ((!outA & !nWA & selA2) ? ~nDA : 0) |
+                 ((!outB & !nWB & selB2) ? ~nDB : 0) |
+                 ((!outC & !nWC & selC2) ? ~nDC : 0) |
+                 ((!outX & !nWX & selX2) ? ~nDX : 0);
+always @(*)
+    if (en3)
+        RG[3] <= ((!outA & !nWA & selA3) ? ~nDA : 0) |
+                 ((!outB & !nWB & selB3) ? ~nDB : 0) |
+                 ((!outC & !nWC & selC3) ? ~nDC : 0) |
+                 ((!outX & !nWX & selX3) ? ~nDX : 0);
 
-// Latch RG1
-always @(*)
-    if (sel1)
-        RG[1] <= ((AA == 1) & !nWA) ? DA :
-                 ((AB == 1) & !nWB) ? DB :
-                 ((AC == 1) & !nWC) ? DC :
-                 ((AX == 1) & !nWX) ? DX : RG[1];
-// Latch RG2
-always @(*)
-    if (sel2)
-        RG[2] <= ((AA == 2) & !nWA) ? DA :
-                 ((AB == 2) & !nWB) ? DB :
-                 ((AC == 2) & !nWC) ? DC :
-                 ((AX == 2) & !nWX) ? DX : RG[2];
-// Latch RG3
-always @(*)
-    if (sel3)
-        RG[3] <= ((AA == 3) & !nWA) ? DA :
-                 ((AB == 3) & !nWB) ? DB :
-                 ((AC == 3) & !nWC) ? DC :
-                 ((AX == 3) & !nWX) ? DX : RG[3];
+// Read RGi.
+assign onDA = !outA ? 4'hz :
+              selA0 ? ~RG[0] :
+              selA1 ? ~RG[1] :
+              selA2 ? ~RG[2] :
+              selA3 ? ~RG[3] : 4'hf;
+assign onDB = !outB ? 4'hz :
+              selB0 ? ~RG[0] :
+              selB1 ? ~RG[1] :
+              selB2 ? ~RG[2] :
+              selB3 ? ~RG[3] : 4'hf;
+assign onDC = !outC ? 4'hz :
+              selC0 ? ~RG[0] :
+              selC1 ? ~RG[1] :
+              selC2 ? ~RG[2] :
+              selC3 ? ~RG[3] : 4'hf;
+assign onDX = !outX ? 4'hz :
+              selX0 ? ~RG[0] :
+              selX1 ? ~RG[1] :
+              selX2 ? ~RG[2] :
+              selX3 ? ~RG[3] : 4'hf;
 
-// TODO: something is wrong with the above.
-// We have five asynchronous processes accessing the same memory.
-// Need at least an arbiter here.
+// Counter overflow output.
+assign nCO = !CO | nCI;
 
 // Compare RG0 and RG3.
 assign E = (RG[0] == RG[3]);
--- tests/k1802bb1/test_k1802bb1.sv
+++ tests/k1802bb1/test_k1802bb1.sv
@@ -1,5 +1,197 @@
 module testbench();
 
-//TODO
+    // Inputs.
+    logic [3:0] nDA, nDB, nDC, nDX;
+    logic [1:0] AA, AB, AC, AX;
+    logic nECA, nECB, nECC, nECX;
+    logic nWA, nWB, nWC, nWX;
+    logic nRA, nRB, nRC, nRX;
+    logic nCI;
+
+    // Outputs.
+    logic [3:0] out_nDA, out_nDB, out_nDC, out_nDX;
+    logic nCO, E;
+
+    bit [3:0] n;
+
+    k1802bb1 dut(
+        nDA, out_nDA, nDB, out_nDB, nDC, out_nDC, nDX, out_nDX,
+        AA, AB, AC, AX,             // address
+        nECA, nECB, nECC, nECX,     // enable channel
+        nWA, nWB, nWC, nWX,         // write strobe
+        nRA, nRB, nRC, nRX,         // read strobe
+        nCI,                        // counter input
+        nCO,                        // counter input
+        E);                         // compare output
+
+    // Status
+    bit fail;
+
+    // Read value from the variable.
+    task verify(input check, input string message);
+        if (!check) begin
+            $display ("%s", message);
+            fail = 1;
+        end
+    endtask
+
+    task verify4(logic [3:0] rval, logic [3:0] val, input string message);
+        if (rval !== val) begin
+            $display ("Assert %s: 'b%04b !== 'b%04b", message, rval, val);
+            fail = 1;
+        end
+    endtask
+
+    // Write data to register RG0-RG3 via port A..X.
+    task write_A(logic [1:0] r, logic [3:0] val);
+        AA = r; nDA = ~val; nECA = 0; nWA = 0; #1; nWA = 1; #1; nECA = 1; #1;
+    endtask
+    task write_B(logic [1:0] r, logic [3:0] val);
+        AB = r; nDB = ~val; nECB = 0; nWB = 0; #1; nWB = 1; #1; nECB = 1; #1;
+    endtask
+    task write_C(logic [1:0] r, logic [3:0] val);
+        AC = r; nDC = ~val; nECC = 0; nWC = 0; #1; nWC = 1; #1; nECC = 1; #1;
+    endtask
+    task write_X(logic [1:0] r, logic [3:0] val);
+        AX = r; nDX = ~val; nECX = 0; nWX = 0; #1; nWX = 1; #1; nECX = 1; #1;
+    endtask
+
+    // Read data from register RG0-RG3 via port A..X, and verify.
+    task read_A(logic [1:0] r, logic [3:0] val);
+        logic [3:0] rval;
+        AA = r; nECA = 0; nRA = 0; #1; rval = ~out_nDA; #1; nRA = 1; nECA = 1; #1;
+        verify4(rval, val, "read_A");
+    endtask
+    task read_B(logic [1:0] r, logic [3:0] val);
+        logic [3:0] rval;
+        AB = r; nECB = 0; nRB = 0; #1; rval = ~out_nDB; #1; nRB = 1; nECB = 1; #1;
+        verify4(rval, val, "read_B");
+    endtask
+    task read_C(logic [1:0] r, logic [3:0] val);
+        logic [3:0] rval;
+        AC = r; nECC = 0; nRC = 0; #1; rval = ~out_nDC; #1; nRC = 1; nECC = 1; #1;
+        verify4(rval, val, "read_C");
+    endtask
+    task read_X(logic [1:0] r, logic [3:0] val);
+        logic [3:0] rval;
+        AX = r; nECX = 0; nRX = 0; #1; rval = ~out_nDX; #1; nRX = 1; nECX = 1; #1;
+        verify4(rval, val, "read_X");
+    endtask
+
+    // Read and verify values of registers RG0-RG3.
+    task read_and_verify();
+        read_A(0, n);           // Read A: RG0 -> n
+        read_B(1, n+1);         //      B: RG1 -> n+1
+        read_C(2, n+2);         //      C: RG2 -> n+2
+        read_X(3, n+3);         //      D: RG3 -> n+3
+
+        read_A(1, n+1);         // Read A: RG1 -> n+1
+        read_B(2, n+2);         //      B: RG2 -> n+2
+        read_C(3, n+3);         //      C: RG3 -> n+3
+        read_X(0, n);           //      D: RG0 -> n
+
+        read_A(2, n+2);         // Read A: RG2 -> n+2
+        read_B(3, n+3);         //      B: RG3 -> n+3
+        read_C(0, n);           //      C: RG0 -> n
+        read_X(1, n+1);         //      D: RG1 -> n+1
+
+        read_A(3, n+3);         // Read A: RG3 -> n+3
+        read_B(0, n);           //      B: RG0 -> n
+        read_C(1, n+1);         //      C: RG1 -> n+1
+        read_X(2, n+2);         //      D: RG2 -> n+2
+    endtask
+
+initial begin
+$display("------------------------");
+$dumpfile("test.vcd");
+$dumpvars();
+fail = 0;
+
+//------------------------
+
+// Initial state
+nDA <= 'b1111;
+nDB <= 'b1111;
+nDC <= 'b1111;
+nDX <= 'b1111;
+AA <= 'b11;
+AB <= 'b11;
+AC <= 'b11;
+AX <= 'b11;
+nECA <= 1;
+nECB <= 1;
+nECC <= 1;
+nECX <= 1;
+nWA <= 1;
+nWB <= 1;
+nWC <= 1;
+nWX <= 1;
+nRA <= 1;
+nRB <= 1;
+nRC <= 1;
+nRX <= 1;
+nCI <= 0;
+#1;
+
+verify(out_nDA === 4'hz, "Assert 0:  out_nDA !== 4'hz");    // Vector No: 0
+verify(out_nDB === 4'hz, "Assert 1: out_nDB !== 4'hz");
+verify(out_nDC === 4'hz, "Assert 2: out_nDC !== 4'hz");
+verify(out_nDX === 4'hz, "Assert 3: out_nDX !== 4'hz");
+#1;
+
+//------------------------
+n = 0;
+write_A(0, n);          // Write A: n   -> RG0
+write_B(1, n+1);        //       B: n+1 -> RG1
+write_C(2, n+2);        //       C: n+2 -> RG2
+write_X(3, n+3);        //       D: n+3 -> RG3
+
+verify(nCO === 1, "Assert 4: nCO !== 0");
+verify(E === 0, "Assert 5: E !== 0");
+
+read_and_verify();
+
+//------------------------
+n += 3;
+write_A(1, n+1);        // Write A: n+1 -> RG1
+write_B(2, n+2);        //       B: n+2 -> RG2
+write_C(3, n+3);        //       C: n+3 -> RG3
+write_X(0, n);          //       D: n   -> RG0
+
+read_and_verify();
+
+//------------------------
+n += 7;
+write_A(2, n+2);        // Write A: n+2 -> RG2
+write_B(3, n+3);        //       B: n+3 -> RG3
+write_C(0, n);          //       C: n   -> RG0
+write_X(1, n+1);        //       D: n+1 -> RG1
+
+read_and_verify();
+
+//------------------------
+n += 9;
+write_A(3, n+3);        // Write A: n+3 -> RG3
+write_B(0, n);          //       B: n   -> RG0
+write_C(1, n+1);        //       C: n+1 -> RG1
+write_X(2, n+2);        //       D: n+2 -> RG2
+
+read_and_verify();
+
+//------------------------
+//TODO: check counter mode
+
+//------------------------
+#1;
+if (fail) begin
+    $display("Test FAIL");
+    $display("------------------------");
+    $finish(1);
+end
+$display("Test PASS");
+$display("------------------------");
+$finish;
+
+end
 
 endmodule
