d71968c89c58840fbaa7123d8f7128b7df05108c
Serge Vakulenko <serge.vakulenko@gmail.com>
2016-10-13 00:11:55 -0700

    Create cpu.sv

---
+++ rtl/cpu.sv
@@ -0,0 +1,14 @@
+// ALU:
+// ALUS -> I[2:0]
+// FUNC -> I[5:3]
+// ALUD -> I[8:6]
+// RA -> A
+// RB -> B
+// H  -> mode32
+// CI mux -> C0
+
+// Status/Shifts:
+// SHMUX -> I[9:6]
+// STOPC -> I[5:0]
+// CEM   -> ~nCEM
+// CEN   -> ~nCEN
