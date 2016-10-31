307b5e7ec8a2f6c4d97822442dd3bc7355eab94f
Serge <serge@vak.ru>
2016-10-30 23:25:29 -0700

    Update arbiter.

--- rtl/arbiter.sv
+++ rtl/arbiter.sv
@@ -5,39 +5,141 @@
 //
 module arbiter(
     input  wire        clk,
-    input  wire        reset,
-    input  wire  [3:0] i_request,   // input request
+    input  wire  [3:0] request, // input request
 
-    output logic [1:0] o_arx,       // busio register index
-    output logic       o_ecx,       // busio port enable
-    output logic       o_wrx,       // busio write enable
-    output logic       o_astb,      // memory address strobe
-    output logic       o_rd,        // memory read
-    output logic       o_wr,        // memory write
-    output logic       o_done       // resulting acknowledge
+    output logic [1:0] arx,     // busio register index
+    output logic       ecx,     // busio port enable
+    output logic       wrx,     // busio write enable
+    output logic       astb,    // memory address strobe
+    output logic       rd,      // memory read
+    output logic       wr,      // memory write
+    output logic       done     // resulting acknowledge
 );
 timeunit 1ns / 10ps;
 
-always @(posedge clk)
-    if (reset) begin
-        // Reset everything
-        o_arx <= 2'b11;
-        o_ecx <= 0;
-        o_wrx <= 0;
-        o_astb <= 0;
-        o_rd <= 0;
-        o_wr <= 0;
-        o_done <= 1;
-    end
-    else if (i_request == 0) begin
-        // Clear previous request
-        o_done <= 0;
-    end
-    else if (i_request != 0 && o_done == 0) begin
-        //TODO: start new operation
-        if (testbench.tracefd)
-            $fdisplay(testbench.tracefd, "(%0d) *** Arbiter op=%0d not implemented yet!", $time, i_request);
-        o_done <= 1;
-    end
+logic [6:0] step;
+
+parameter MAXSTATE = 'h7f;
+
+//
+// Sequential state transition
+//
+always_ff @(posedge clk)
+    if (request == 0)
+        step <= 0;
+    else if (step != MAXSTATE)         // TODO need some action on timeout
+        step <= step + 1;
+
+//
+// Output assignments
+//
+always_ff @(posedge clk)
+    case (request)
+    0:  // Reset everything
+        {arx, ecx, wrx, astb, rd, wr, done} = {2'b11, 0, 0, 0, 0, 0, 0};
+
+    1:  // CCRD, чтение кэша команд
+        case (step)
+              0: //TODO
+              1: //TODO
+              2: //TODO
+        default: {arx, ecx, wrx, astb, rd, wr, done} = {2'b11, 0, 0, 0, 0, 0, 0};
+        endcase
+
+    2:  // CCWR, запись в кэш команд
+        case (step)
+              0: //TODO
+              1: //TODO
+              2: //TODO
+        default: {arx, ecx, wrx, astb, rd, wr, done} = {2'b11, 0, 0, 0, 0, 0, 0};
+        endcase
+
+    3:  // DCRD, чтение кэш операндов
+        case (step)
+              0: //TODO
+              1: //TODO
+              2: //TODO
+        default: {arx, ecx, wrx, astb, rd, wr, done} = {2'b11, 0, 0, 0, 0, 0, 0};
+        endcase
+
+    4:  // DCWR, запись в кэш операндов
+        case (step)
+              0: //TODO
+              1: //TODO
+              2: //TODO
+        default: {arx, ecx, wrx, astb, rd, wr, done} = {2'b11, 0, 0, 0, 0, 0, 0};
+        endcase
+
+    8:  // FЕТСН, чтение команды
+        case (step)
+              0: //TODO
+              1: //TODO
+              2: //TODO
+        default: {arx, ecx, wrx, astb, rd, wr, done} = {2'b11, 0, 0, 0, 0, 0, 0};
+        endcase
+
+    9:  // DRD, чтение операнда
+        case (step)
+              0: //TODO
+              1: //TODO
+              2: //TODO
+        default: {arx, ecx, wrx, astb, rd, wr, done} = {2'b11, 0, 0, 0, 0, 0, 0};
+        endcase
+
+    10: // DWR, запись результата
+        case (step)
+              0: //TODO
+              1: //TODO
+              2: //TODO
+        default: {arx, ecx, wrx, astb, rd, wr, done} = {2'b11, 0, 0, 0, 0, 0, 0};
+        endcase
+
+    11: // RDMWR, чтение - модификация - запись (семафорная)
+        case (step)
+              0: //TODO
+              1: //TODO
+              2: //TODO
+        default: {arx, ecx, wrx, astb, rd, wr, done} = {2'b11, 0, 0, 0, 0, 0, 0};
+        endcase
+
+    12: // BTRWR, запись в режиме блочной передачи
+        case (step)
+              0: //TODO
+              1: //TODO
+              2: //TODO
+        default: {arx, ecx, wrx, astb, rd, wr, done} = {2'b11, 0, 0, 0, 0, 0, 0};
+        endcase
+
+    13: // BTRRD, чтение в режиме блочной передачи
+        case (step)
+              0: //TODO
+              1: //TODO
+              2: //TODO
+        default: {arx, ecx, wrx, astb, rd, wr, done} = {2'b11, 0, 0, 0, 0, 0, 0};
+        endcase
+
+    14: // BICLR, сброс прерываний на шине
+        case (step)
+              0: //TODO
+              1: //TODO
+              2: //TODO
+        default: {arx, ecx, wrx, astb, rd, wr, done} = {2'b11, 0, 0, 0, 0, 0, 0};
+        endcase
+
+    15: // BIRD, чтение прерываний с шины
+        case (step)
+              0: //TODO
+              1: //TODO
+              2: //TODO
+        default: {arx, ecx, wrx, astb, rd, wr, done} = {2'b11, 0, 0, 0, 0, 0, 0};
+        endcase
+
+    default: begin
+            //TODO: Unknown request
+            if (testbench.tracefd)
+                $fdisplay(testbench.tracefd, "(%0d) *** Wrong arbiter op=%0d!", $time, request);
+            {arx, ecx, wrx, astb, rd, wr, done} = {2'b11, 0, 0, 0, 0, 0, 0};
+        end
+    endcase
 
 endmodule
