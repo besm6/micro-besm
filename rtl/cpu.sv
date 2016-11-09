b2663131fa0c73088bc02ac8d549e01ccbfbdba0
Serge Vakulenko <serge.vakulenko@gmail.com>
2016-11-08 22:12:41 -0800

    Update sectest.

--- rtl/cpu.sv
+++ rtl/cpu.sv
@@ -307,13 +307,12 @@ always @(posedge clk)
 logic       tm_clk;                 // clock for timer, 20 times slower
 logic [4:0] tm_counter;             // clock divider
 logic [7:0] tm_dout;                // data output bus
-logic [2:0] tm_event;               // timer outputs
 
 wire tm_cs = (DDEV == 4);           // CTIME: chip select
 wire tm_wr = (DSRC == 15) & WRD;    // WT: write strobe from Y bus
 wire tm_rd = (DSRC == 14);          // RT: read strobe to Y bus
 
-i8253 timer(tm_clk, tm_cs, tm_rd, tm_wr, FFCNT[1:0], Y[7:0], tm_dout, tm_event);
+i8253 timer(tm_clk, tm_cs, tm_rd, tm_wr, FFCNT[1:0], Y[7:0], tm_dout, /*out0*/, /*out1*/, /*out2*/);
 
 // Clock divider by 20.
 always @(posedge clk) begin
